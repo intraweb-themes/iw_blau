4415271baf46dbcd582a5c3e190438b95c9233c9
fcasanellas <fcasanellas@e968f4be-1634-4921-b96b-e3dd0a78059a>
2010-10-27 09:37:39 +0000

    new SVN structure

---
+++ version.php
@@ -0,0 +1,30 @@
+<?php 
+/**
+ * PostNuke Application Framework
+ *
+ * @copyright (c) 2006, Toni Ginard
+ * @link aginard@xtec.cat
+ * @version $Id: version.htm 22696 2007-09-18 08:38:55Z markwest $
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @author Toni Ginard
+ * @package PostNuke_Themes
+ * @subpackage iw_blau
+ */
+$themeversion['name'] 	= 'iw_blau';
+$domain = ZLanguage::getThemeDomain('theme_iw_blau');
+$themeversion['url'] = __('iw_blau', $domain);
+$themeversion['displayname'] = __('Intraweb blau', $domain);
+$themeversion['description'] = __('Theme developed by the Intraweb project staff', $domain);
+$themeversion['regid'] = 0;
+$themeversion['version'] = '1.0';
+$themeversion['official'] = 0;
+$themeversion['author'] = 'Toni Ginard';
+$themeversion['contact'] = 'toni.ginard@gmail.com';
+$themeversion['admin'] = 1;
+$themeversion['user'] = 1;
+$themeversion['system'] = 1;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'GNU/GPL';
+$themeversion['xhtml'] = 1;
